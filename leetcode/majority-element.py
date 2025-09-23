1db65ad5ca7a35240b7780342bf2476bdbd2d30f
dldzurev <dldzurev@uwaterloo.ca>
2025-09-22 23:35:25 -0400

    Update solution for majority-element

--- leetcode/majority-element.py
+++ leetcode/majority-element.py
@@ -1,23 +1,19 @@
-import math
-class Solution(object):
-    def majorityElement(self, nums):
-        target = (len(nums)/2)+1
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
-        appear = 1
-        i = 0
-        while(appear < target):
-            if( (i > 0) and (nums[i] == nums[i-1])):
-                appear += 1
-                i+=1
-            else:
-                appear = 1
-                i+=1
-                
-
- 
-        return nums[i-1]
-
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        curr_max = 0
+        curr_max_element = nums[0]
+        curr_candidate = nums[0]
+        curr_count = 1
+        
+        for i in range(len(nums)):
+            if(i > 0):
+                if(nums[i] == curr_candidate):
+                    curr_count += 1
+                    if(curr_count > curr_max):
+                        curr_max_element = nums[i]
+                        curr_max = curr_count
+                else:
+                    curr_candidate = nums[i]
+                    curr_count = 1
+        return curr_max_element
