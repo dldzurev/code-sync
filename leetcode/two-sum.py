97871a2ee611c36cc90ca2462d7758b8697988bb
dldzurev <dldzurev@uwaterloo.ca>
2025-09-23 23:59:57 -0400

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,1 +1,8 @@
-9123456›[2,7,11,15]9[3,2,4]6[3,3]6
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+
+        return []
