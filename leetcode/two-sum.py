a517d63508189fcb5b39ccdeeac41e9246f46289
dldzurev <dldzurev@uwaterloo.ca>
2025-11-04 23:53:55 -0500

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
         seen = {}
-        for i in range(len(nums)):
-            need = target - nums[i]
-            if(need in seen):
-                return i, seen[need]
-            else:
-                seen[nums[i]] = i
+        i = 0
+        for x in nums:
+            need = target - x
+            if need in seen: return i, seen[need]
+            else: seen[x] = i
+            i += 1
