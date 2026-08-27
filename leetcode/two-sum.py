cf1bebb20422e0be48f32ddf4a709ead8821ea8b
dldzurev <dldzurev@uwaterloo.ca>
2026-08-27 10:39:15 -0700

    Update solution for two-sum

--- leetcode/two-sum.py
+++ leetcode/two-sum.py
@@ -1,11 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        op = []
-        seen  = {} # key = val, val = index
-        for i in range(len(nums)):
-            need = target - nums[i] 
-            if need in seen:
-                op.extend((i,seen[need]))
-            seen[nums[i]] = i
-
-        return op
+        seen = {}
+        for index,num in enumerate(nums):
+            need = target - num
+            if need in seen:return index,seen[need]
+            else: seen[num] = index
