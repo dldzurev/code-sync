22afe8c215ea80671fd406e66dc81f4b68b22d92
dldzurev <dldzurev@uwaterloo.ca>
2026-08-27 14:23:22 -0700

    Update solution for longest-consecutive-sequence

--- leetcode/longest-consecutive-sequence.py
+++ leetcode/longest-consecutive-sequence.py
@@ -1,18 +1,19 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        curr_max = 1
-        curr_count = 1
         nums.sort()
-        if(len(nums) == 0):
-            return 0
-        for i in range(1,len(nums),1):
-            if (nums[i-1] + 1) == nums[i] :
-                curr_count += 1
-                if curr_count > curr_max: curr_max = curr_count
-            elif nums[i-1] == nums[i]:
-                continue
-            else:
+        print(nums)
+        curr_count = 1
+        if len(nums) == 0: return 0
+        prev = nums[0]
+        seq_freq = []
+        heapq.heapify(seq_freq)
+        for i in range(1,len(nums)):
+
+            if ((nums[i] == prev+1 or nums[i] == prev-1)) :
+                curr_count+=1
+            elif(prev != nums[i]):
+                heapq.heappush(seq_freq,-curr_count)
                 curr_count = 1
-            
-            
-        return curr_max
+            prev = nums[i]
+        heapq.heappush(seq_freq,-curr_count)
+        return -seq_freq[0]
