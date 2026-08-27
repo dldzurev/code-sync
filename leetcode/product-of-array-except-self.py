3f7be20248f6af5a3f42ba073548c042081bd53e
dldzurev <dldzurev@uwaterloo.ca>
2026-08-27 13:18:28 -0700

    Update solution for product-of-array-except-self

--- leetcode/product-of-array-except-self.py
+++ leetcode/product-of-array-except-self.py
@@ -1,12 +1,18 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
-        prefix = 1
-        for i in range (len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
-        postfix =1
-        for i in range(len(nums) -1,-1,-1):
-            res[i] *= postfix
-            postfix *= nums[i]
-        return res
+        #[1,1,2,6]
+        #[24,12,4,1]
+        list_length = len(nums)
+        left_list = [1] * list_length
+        right_list = [1] * list_length
+        for i in range (list_length):
+            #populate left
+
+            if(i>0):
+                left_list[i] = left_list[i-1]*nums[i-1]
+            
+                right_list[list_length -1 -i] = right_list[list_length -i] * nums[list_length -i]
+
+        for i in range(list_length):
+            nums[i] = left_list[i]*right_list[i]
+        return nums
