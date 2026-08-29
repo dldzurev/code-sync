5e75c0c67c97cbf17161074647d0a80b5c9a5906
dldzurev <dldzurev@uwaterloo.ca>
2026-08-29 08:51:35 -0700

    Update solution for two-sum-ii-input-array-is-sorted

--- leetcode/two-sum-ii-input-array-is-sorted.py
+++ leetcode/two-sum-ii-input-array-is-sorted.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        need = {}
-        for i in range (len(numbers)):
-            need_ = target - numbers[i] 
-            if(need_ in need):
-                return need[need_] + 1, i + 1
+        seen = {}
+        for index,number in enumerate(numbers):
+            need = target - number
+            if need in seen:
+                return [seen[need] + 1,index + 1]
             else:
-                need[numbers[i]] = i
+                seen[number] = index
