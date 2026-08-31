c3134fc25472fc4ad3b2b1088cfc31331427ce0d
dldzurev <dldzurev@uwaterloo.ca>
2026-08-31 06:22:58 -0700

    Update solution for balanced-binary-tree

--- leetcode/balanced-binary-tree.py
+++ leetcode/balanced-binary-tree.py
@@ -7,15 +7,9 @@
 class Solution:
     def isBalanced(self, root):
         def dfs(root):
-            if root is None:
-                return [True, 0]  
+            if not root: return[True,0]
             left = dfs(root.left)
             right = dfs(root.right)
-            balanced = (
-                left[0]
-                and right[0]
-                and abs(left[1] - right[1]) <= 1
-            )
-            height = 1 + max(left[1], right[1])
-            return [balanced, height]
+            balanced = left[0] and right[0] and abs(left[1]-right[1]) <=1
+            return [balanced, 1 + max(left[1],right[1])]
         return dfs(root)[0]
