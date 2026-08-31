1c54bd0ded11906e1d29e601bee137d1e29fa9a1
dldzurev <dldzurev@uwaterloo.ca>
2026-08-30 18:39:49 -0700

    Update solution for search-in-rotated-sorted-array

--- leetcode/search-in-rotated-sorted-array.py
+++ leetcode/search-in-rotated-sorted-array.py
@@ -1,21 +1,17 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        #1 <= k <= len(nums). 2
-        #| | |
-        #5,1,3
         left = 0
         right = len(nums)-1
         while(left <= right):
             mid  = left + (right-left)//2
             if(nums[mid] == target):
                 return mid
-            if(nums[left] <= nums[mid]): #left sorted mins element is at left
+            if(nums[left] <= nums[mid]):
                 if (nums[left] <= target < nums[mid]):
                     right = mid -1
                 else:
                     left = mid + 1
-
-            elif(nums[right] >= nums[mid]):# right sorted min element is at mid
+            else:
                 if(nums[right] >= target > nums[mid]):
                     left = mid+1
                 else:
