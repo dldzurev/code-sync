ec2172bcd93d76f7457884a8ebb11737552ab0ae
dldzurev <dldzurev@uwaterloo.ca>
2026-09-09 12:02:13 -0700

    Update solution for remove-element

--- leetcode/remove-element.py
+++ leetcode/remove-element.py
@@ -1,13 +1,10 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        occurences = 0
-        i = len(nums) - 1
-
-        while(i >= 0):
-            if(nums[i] == val ):
-                occurences += 1
-                nums[i] = nums[len(nums) - occurences]
-                
-
-            i = i - 1
-        return len(nums) - occurences
+        left = len(nums) -1 #3
+        right  = left #3
+        while(left >= 0):#3>0
+            if nums[left] == val:
+                nums[left],nums[right] = nums[right],nums[left]
+                right -= 1
+            left -= 1
+        return right+1
