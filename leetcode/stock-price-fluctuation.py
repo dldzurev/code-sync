345131ba0a889bae61eb2c6ba872a76586fc5701
dldzurev <dldzurev@uwaterloo.ca>
2026-09-18 12:21:03 -0700

    Update solution for stock-price-fluctuation

--- leetcode/stock-price-fluctuation.py
+++ leetcode/stock-price-fluctuation.py
@@ -1,16 +1,11 @@
 class StockPrice:
-    #a stream of records about a stock each record contains a time stamp corresponding to the curr price
-    #records are not in order and some are incorect
-    #Another record with the same timestamp may appear later in the stream correcting the price of the previous wrong record.
     def __init__(self):
-        #min heap              max heap                dict
-        #min price:timestamp.  max price:timestamp     time_stamp : price 
         self.price_dict = {}
         self.min_price = []
         self.max_price = []
         self.last_time = 0
 
-    def update(self, timestamp: int, price: int) -> None:#the price of the stock at a times 
+    def update(self, timestamp: int, price: int) -> None:
         self.last_time = max(self.last_time,timestamp)
         self.price_dict[timestamp] = price
         heapq.heappush(self.max_price,[-price,timestamp])
@@ -35,12 +30,3 @@ class StockPrice:
                 return p
             heapq.heappop(self.min_price)
         return
-        
-
-
-# Your StockPrice object will be instantiated and called as such:
-# obj = StockPrice()
-# obj.update(timestamp,price)
-# param_2 = obj.current()
-# param_3 = obj.maximum()
-# param_4 = obj.minimum()
