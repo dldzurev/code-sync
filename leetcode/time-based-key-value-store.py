129ae57ec039904681715640e372ceb411cddcc6
dldzurev <dldzurev@uwaterloo.ca>
2026-09-18 13:48:16 -0700

    Update solution for time-based-key-value-store

--- leetcode/time-based-key-value-store.py
+++ leetcode/time-based-key-value-store.py
@@ -1,37 +1,25 @@
 class TimeMap:
 
     def __init__(self):
-        self.key_to_vals = {}
+        self.my_dict = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key in self.key_to_vals:
-            self.key_to_vals[key].append([timestamp,value])
+        if key in self.my_dict:
+            self.my_dict[key].append([timestamp,value])
         else:
-            self.key_to_vals[key] = [[timestamp,value]]
-    #1 3 5 7 9
-    def get(self, key: str, timestamp: int) -> str:
-        if key not in self.key_to_vals: return ""
-        search_list = self.key_to_vals[key]
-        if(search_list[0][0] > timestamp): return ""
-        left = 0
-        right = len(search_list) - 1
-        result = ""
-        while(left <= right):
-            mid = left + (right - left)//2
-            if(search_list[mid][0] == timestamp):
-                return search_list[mid][1]
-            if(search_list[mid][0] > timestamp):
-                right = mid-1
-            else:
-                result = search_list[mid][1]
-                left = mid+1
-        return result
+            self.my_dict[key] = [[timestamp,value]]
 
-        
+    def get(self, key: str, timestamp: int) -> str:
+        if key in self.my_dict and len(self.my_dict[key])>0:
+            for i in range(len(self.my_dict[key])-1,-1,-1):
+                if self.my_dict[key][i][0] <= timestamp:
+                    return self.my_dict[key][i][1]
+                
+        return ""
         
 
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
-# obj.set(key,value,timestamp)
-# param_2 = obj.get(key,timestamp)
+# obj.set(key, value, timestamp)
+# param_2 = obj.get(key, timestamp)
