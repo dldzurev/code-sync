bb2f28bfb88d3ea802f518439cb75c9afe2e2136
dldzurev <dldzurev@uwaterloo.ca>
2026-09-20 16:24:00 -0700

    Update solution for task-scheduler

--- leetcode/task-scheduler.py
+++ leetcode/task-scheduler.py
@@ -1,20 +1,25 @@
 class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
-        t = 0
-        count = Counter(tasks)
-        #freq,task
-        heap = [-cnt for cnt in count.values()]
-        heapq.heapify(heap)
-        #freq,when run
-        queue = deque()
-        while queue or heap:
-            while(queue and queue[0][1] <= t):
-                heapq.heappush(heap,queue.popleft()[0])
-            if heap:
-                run = heapq.heappop(heap)
-                run += 1
-                if run < 0:
-                    queue.append([run,t+n+1])
+        cycles = 0 
+        n=n+1
+        remaining = {}
+        heap = []
+        wait = deque()#freq,cooldown
+        for task in tasks:
+            remaining[task] = 1 + remaining.get(task,0)
+        print(remaining)
+        for task in remaining:
+            heapq.heappush(heap,[-remaining[task],0])#freq,cooldown
+        while(heap or wait):
+            while wait and wait[0][1] <= cycles:
+                freq,cool = wait.popleft()
+                heapq.heappush(heap,[-(freq),0])
+            if(heap):
+                freq,cool = heapq.heappop(heap)
+                freq = -freq
+                if freq > 1:
+                    wait.append([freq-1,cycles+n])
 
-            t +=1
-        return t
+                    
+            cycles+=1
+        return cycles
