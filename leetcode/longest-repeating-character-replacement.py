b616081fa316dd21cd9373617aeb98bf581644f0
dldzurev <dldzurev@uwaterloo.ca>
2026-09-20 14:00:08 -0700

    Update solution for longest-repeating-character-replacement

--- leetcode/longest-repeating-character-replacement.py
+++ leetcode/longest-repeating-character-replacement.py
@@ -1,19 +1,15 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-     #   ABAB
-     #k = 2
-        char_freq = defaultdict(int)
-        start = 0
-        end = 0
-        max_ = 0
-        while(end < len(s)):
-            size = end-start + 1 
-            char_freq[s[end]]+=1
-            while(size - max(char_freq.values()) > k):
-                char_freq[s[start]] -=1
-                start +=1
-                size -=1
-            else:
-                end +=1
-            max_ = max(max_,size)
-        return max_
+        count = {}
+        left = 0
+        res = 0
+        curr = 0
+        for i in range(len(s)):
+            count[s[i]] = 1 + count.get(s[i],0)
+            curr+=1
+            while(curr - max(count.values()) > k):
+                curr -=1
+                count[s[left]] -=1
+                left +=1
+            res = max(res,curr)
+        return res
