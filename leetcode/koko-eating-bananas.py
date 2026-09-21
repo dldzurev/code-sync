65bef814a6cf7fa71969e83d31973d8095292312
dldzurev <dldzurev@uwaterloo.ca>
2026-09-20 17:28:07 -0700

    Update solution for koko-eating-bananas

--- leetcode/koko-eating-bananas.py
+++ leetcode/koko-eating-bananas.py
@@ -1,16 +1,15 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def hours_needed(k, piles):
-            hrs = 0
+        l,r=1, max(piles)
+        min_speed = r
+        while(l<=r):
+            k = (l+r)//2
+            curr_h = 0
             for pile in piles:
-                hrs += math.ceil(pile / k)
-            return hrs
-        prevk1 = max(piles)  # high
-        prevk2 = 1           # low
-        while prevk2 <= prevk1:
-            k = prevk2 + (prevk1 - prevk2) // 2
-            if hours_needed(k, piles) <= h:
-                prevk1 = k - 1
+                curr_h += math.ceil(pile / k)
+            if curr_h <= h:
+                min_speed = min(min_speed,k)
+                r = k-1
             else:
-                prevk2 = k + 1
-        return prevk2
+                l = k+1
+        return min_speed
