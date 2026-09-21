4cbb58e0908d92686e200f5d4aab8672dd349d4e
dldzurev <dldzurev@uwaterloo.ca>
2026-09-20 17:01:18 -0700

    Update solution for top-k-frequent-elements

--- leetcode/top-k-frequent-elements.py
+++ leetcode/top-k-frequent-elements.py
@@ -1,14 +1,12 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = {}
-        freq = [[] for _ in range(len(nums)+1)]
-        res = []
         for num in nums:
-            count[num] = 1 + count.get(num,0)
-        for n,c in count.items():
-            freq[c].append(n)
-        for i in range(len(freq) - 1 ,-1,-1):
-            for j in range(len(freq[i])):
-                res.append(freq[i][j])
-                if len(res) == k:
-                    return res
+            count[num] = 1+ count.get(num,0) 
+            heap = []
+        for c in count:
+            heapq.heappush(heap,[-count[c],c])
+        ret = []
+        for i in range(k):
+            ret.append(heapq.heappop(heap)[1])
+        return ret
